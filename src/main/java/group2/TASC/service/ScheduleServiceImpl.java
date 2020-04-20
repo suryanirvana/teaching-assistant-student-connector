4164babb0d20c3fe7b20d694d9f3bccf52f57327
Tsamara Esperanti Erwin <sarah.esperanti@gmail.com>
2020-04-20 16:08:36 +0700

    Add max no of student to TA appointment, edit time display

--- src/main/java/group2/TASC/service/ScheduleServiceImpl.java
+++ src/main/java/group2/TASC/service/ScheduleServiceImpl.java
@@ -5,6 +5,7 @@ import group2.TASC.repository.ScheduleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
