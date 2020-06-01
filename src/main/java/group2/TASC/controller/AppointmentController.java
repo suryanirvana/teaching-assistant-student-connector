4295dcd10112b52efc0ffce87f84a91a665def82
Tsamara Esperanti Erwin <tsamara.esperanti@ui.ac.id>
2020-06-02 00:20:50 +0700

    add mailerservice

--- src/main/java/group2/TASC/controller/AppointmentController.java
+++ src/main/java/group2/TASC/controller/AppointmentController.java
@@ -4,6 +4,7 @@ import group2.TASC.model.Appointment;
 import group2.TASC.model.Schedule;
 import group2.TASC.model.User;
 import group2.TASC.service.AppointmentService;
+import group2.TASC.service.MailerService;
 import group2.TASC.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -27,6 +28,9 @@ public class AppointmentController {
 
     @Autowired
     AppointmentService appointmentService;
+
+    @Autowired
+    MailerService mailerService;
 
     @GetMapping("/seeappointment/{USERNAME}")
     public String appointmentPage(@PathVariable("USERNAME") String username, Authentication authentication, Model model) {
@@ -63,11 +67,9 @@ public class AppointmentController {
         if (result.hasErrors()) {
             return result.toString();
         }
+        mailerService.sendEmail("emailnya TA",  "You have a new appointment request",
+                "Hello TA, student has a new appointment request for course on date at. Please give your confirmation on the application.");
 
-//
-//        mailerService.sendEmail(System.getenv("TEST_EMAIL"), "You have added a new course",
-//                "Successfully add new schedule");
-//
         appointmentService.addAppointment(appointment);
         model.addAttribute("APPOINTMENT", appointmentService.findAllAppointments());
         return "redirect:/seeappointment/" + username;
@@ -78,6 +80,9 @@ public class AppointmentController {
         Appointment appointment = appointmentService.getAppointmentById(id);
         appointmentService.acceptAppointment(appointment);
         model.addAttribute("APPOINTMENT", appointmentService.findAllAppointments());
+
+        mailerService.sendEmail("emailnya student", "Your appointment request is accepted",
+                "Hello student your appointment with TA for course is confirmed and scheduled on date at time. Contact your TA for further information");
         return "redirect:/seeappointment/" + username;
     }
 
@@ -86,6 +91,8 @@ public class AppointmentController {
         Appointment appointment = appointmentService.getAppointmentById(id);
         appointmentService.rejectAppointment(appointment);
         model.addAttribute("APPOINTMENT", appointmentService.findAllAppointments());
+        mailerService.sendEmail("emailnya student", "Your appointment request is rejected",
+                "Hello student your appointment with TA for course is rejected and scheduled on date at time. Please request a new appointment.");
         return "redirect:/seeappointment/" + username;
     }
 }
