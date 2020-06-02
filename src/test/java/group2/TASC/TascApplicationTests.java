b5302b91eb586ab2299e4566bf960072b6b5b4f9
Surya Nirvana Prima <suryanirvanaprima@Suryas-MacBook-Pro.local>
2020-06-02 09:32:13 +0700

    Fixed error for db test

--- src/test/java/group2/TASC/TascApplicationTests.java
+++ src/test/java/group2/TASC/TascApplicationTests.java
@@ -3,10 +3,13 @@ package group2.TASC;
 import static org.assertj.core.api.Assertions.*;
 
 import group2.TASC.controller.MainController;
+import group2.TASC.security.UserDetailsImpl;
+import group2.TASC.security.UserDetailsServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
@@ -15,6 +18,13 @@ class TascApplicationTests {
 
     @Autowired
     MainController mainController;
+
+    @MockBean
+    private UserDetailsImpl userDetails;
+
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
 
     @Test
     void contextLoads() {
